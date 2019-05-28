0dd95385b6199b4b9ef213fd7eb1638865273575
biaol <695769659@qq.com>
2019-05-28 18:42:31 +0800

    【初始化项目】

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    sdfwe sfsdf
+    <div id="allmap" class="allmap"></div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import BMap from "BMap";
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  data: function() {
+    return {};
+  },
+  created() {},
+  mounted() {
+    var map = new BMap.Map("allmap"); // 创建地图实例
+    var point = new BMap.Point(116.331398, 39.897445); // 创建点坐标
+    map.centerAndZoom(point, 12); // 初始化地图，设置中心点坐标和地图级别
+    map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
+
+    // map.addControl(new BMap.NavigationControl());
+    // map.addControl(new BMap.ScaleControl());
+    // map.addControl(new BMap.OverviewMapControl());
+    // map.addControl(new BMap.MapTypeControl());
+    map.setCurrentCity("武汉"); // 仅当设置城市信息时，MapTypeControl的切换功能才能可用
+
+    // var opts = {type: BMAP_NAVIGATION_CONTROL_ZOOM}
+    // map.addControl(new BMap.NavigationControl(opts)); 自定义控件
+
+    map.setMapStyle({ style: "midnight" }); //地图风格
   }
 };
 </script>
+
+<!--Add"scoped" attribute to limit CSS to this component only -->
+<style lang="scss" rel="stylesheet/scss">
+.allmap {
+  height: 300px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+module.exports = {
+  lintOnSave: false,
+  // baseUrl: "/",
+  publicPath: "./",
+  devServer: {
+    port: 8888,
+    proxy: {
+      "/microModule": {
+        target: "http://192.168.1.141:8090/microModule",
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          "^/microModule": "/"
+        }
+      }
+    }
+  },
+
+  //允许对内部的 webpack 配置进行更细粒度的修改。
+  chainWebpack: config => {
+    //命名
+    config.resolve.alias.set("@", resolve("src"));
+    //打包文件带hash
+    config.output.filename("[name].[hash].js").end();
+
+    //为了补删除换行而加的配置
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        // modify the options...
+        options.compilerOptions.preserveWhitespace = true;
+        return options;
+      });
+  },
+  configureWebpack: {
+    externals: {
+      "BMap": "BMap"
+    }
+  }
+};
