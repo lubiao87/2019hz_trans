0a4f65fe4d4a08d168f8f0239605fc3fe9e06682
biaol <695769659@qq.com>
2019-06-19 15:13:29 +0800

    【首页界面】

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,34 +1,156 @@
 <template>
   <div class="home">
-    home页面
-    <div class="router-btn" @click="routerGo('about')">about123</div>
-    <div class="router-btn" @click="routerGo('mapArea')">mapAreay页面</div>
-    <div class="router-btn" @click="routerGo('chatOnline', 1)">我是用户1</div>
-    <div class="router-btn" @click="routerGo('chatOnline', 2)">我是用户2</div>
+    <img class="logo-img" src="../../assets/images/homebg.png" alt="" />
+    <div class="grid-box">
+      <grid :cols="2">
+        <grid-item
+          :label="item.title"
+          v-for="(item, index) in menuList"
+          :key="index"
+          @click.native="routerGo(item)"
+        >
+          <img slot="icon" :src="item.imgSrc" />
+        </grid-item>
+      </grid>
+    </div>
+    <p class="tc foot-stip">
+      Guangdong Yuhui Communication Technology Co., Ltd.
+    </p>
+    <p class="tc foot-stip">provides technical support</p>
   </div>
 </template>
 <script>
-// import BMap from "BMap";
+import { Grid, GridItem, AlertModule } from "vux";
 export default {
+  components: {
+    Grid,
+    GridItem
+  },
   data: function() {
-    return {};
+    return {
+      menuList: [
+        {
+          title: "我要找工程师",
+          routerLink: "mapArea",
+          imgSrc: "./img/chatman.png"
+        },
+        {
+          title: "一键快速报障",
+          routerLink: "home",
+          imgSrc: "./img/chatman.png"
+        },
+        {
+          title: "服务进度查询",
+          routerLink: "home",
+          imgSrc: "./img/chatman.png"
+        },
+        {
+          title: "智慧家庭方案",
+          routerLink: "home",
+          imgSrc: "./img/chatman.png"
+        },
+        {
+          title: "自助查障助手",
+          routerLink: "home",
+          imgSrc: "./img/chatman.png"
+        },
+        {
+          title: "投诉建议反馈",
+          routerLink: "home",
+          imgSrc: "./img/chatman.png"
+        }
+      ]
+    };
   },
   created() {},
-  mounted() {},
+  mounted() {
+    AlertModule.show({
+      title: "温馨提示",
+      content: "您未绑定公众号，请先绑定再参加活动哦",
+      buttonText: "前往绑定",
+      onShow() {
+        console.log("Module: I'm showing");
+      },
+      onHide() {
+        console.log("Module: I'm hiding now");
+      }
+    });
+  },
   methods: {
-    routerGo(name, id) {
-      this.$router.push({ name: name, params: { data: id, id: id } });
+    routerGo(item) {
+      this.$router.push({ name: item.routerLink, params: { data: item } });
     }
   }
 };
 </script>
 
 <!--Add"scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
+<style lang="scss" scoped="">
+@import "@/assets/scss/base.scss"; /*引入配置*/
 .home {
-  font-size: 34px;
+  width: 100%;
+  height: 100%;
+  font-size: 30px;
+  background-color: #289bf0;
+  .logo-img {
+    display: block;
+    width: 100%;
+  }
+  .grid-box {
+    padding-bottom: 20px;
+  }
   .router-btn {
     padding: 20px 30px;
+  }
+  .foot-stip {
+    padding-top: 6px;
+    width: 80%;
+    margin: auto;
+    font-size: 20px;
+    color: $font-color-theme1;
+    opacity: 0.6;
+  }
+}
+</style>
+<style lang="scss">
+@import "@/assets/scss/base.scss"; /*引入配置*/
+.home {
+  .weui-grids:before {
+    border-top: 0.01333rem solid transparent;
+  }
+  .weui-grid:before {
+    border-right: 0.01333rem solid transparent;
+  }
+  .weui-grid:after {
+    border-bottom: 0.01333rem solid transparent;
+  }
+  .weui-grid {
+    padding: 10px;
+    .weui-grid__icon {
+      width: 100%;
+      border-radius: 4px 4px 0 0;
+      height: 150px;
+      overflow: hidden;
+      background-color: $background-color-theme;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        width: 130px;
+        height: 130px;
+      }
+    }
+    .weui-grid__label {
+      padding: 10px;
+      border-radius: 0 0 4px 4px;
+      overflow: hidden;
+      background-color: $background-color-theme;
+    }
+    .weui-grid__icon + .weui-grid__label {
+      margin-top: 0;
+      height: 60px;
+      font-size: 30px;
+    }
   }
 }
 </style>
